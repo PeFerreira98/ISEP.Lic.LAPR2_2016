22e3d87ce89335fc2b8dd96e53a23cb94a540ffa
Sara Silva <1140957@isep.ipp.pt>
2016-06-02 15:24:34 +0100

    Implementação da classe ConfirmarRegistoController

---
+++ src/main/java/lapr/project/controller/ConfirmarRegistoController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.controller;
+
+import java.util.List;
+import lapr.project.model.exhibitions.CentroExposicoes;
+import lapr.project.model.users.Utilizador;
+
+/**
+ *
+ * @author Sara Silva
+ */
+public class ConfirmarRegistoController {
+
+    private final CentroExposicoes m_centro_exposicoes;
+    private Utilizador utilizador;
+
+    public ConfirmarRegistoController(CentroExposicoes centro_exposicoes) {
+        m_centro_exposicoes = centro_exposicoes;
+    }
+
+    public List<Utilizador> getListUtNaoConfirmado() {
+        return m_centro_exposicoes.getRegistoUtilizadores().getListaUtilizadoresNaoRegistados();
+    }
+
+    public void setUtilizador(Utilizador utilizador) {
+        this.utilizador = utilizador;
+    }
+
+    public String utilizadorToString() {
+        return utilizador.toString();
+    }
+
+    public void confirmaUtilizador() {
+        m_centro_exposicoes.getRegistoUtilizadores().registarUtilizador(utilizador);
+    }
+}
