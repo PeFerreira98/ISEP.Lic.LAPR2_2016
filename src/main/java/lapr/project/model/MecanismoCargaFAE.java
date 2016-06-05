5a84f967347d3fc8429fe6c8e3f356275cd1a1ee
Sara Silva <1140957@isep.ipp.pt>
2016-06-05 15:45:51 +0100

    Add class MecanismoCargaFAE [Implementation] [UC-03]

---
+++ src/main/java/lapr/project/model/MecanismoCargaFAE.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model;
+
+import java.util.List;
+import lapr.project.model.exhibitions.Exposicao;
+
+/**
+ *
+ * @author Sara Silva
+ */
+public class MecanismoCargaFAE implements MecanismoAtribuicao {
+
+    private String descricao;
+
+    public MecanismoCargaFAE() {
+        descricao = "Carga FAE";
+    }
+
+    @Override
+    public List<Atribuicao> gerarAtribuicoes(Exposicao exposicao) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
