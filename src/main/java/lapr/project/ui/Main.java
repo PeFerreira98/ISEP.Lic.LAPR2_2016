305df73d24a5b5538291524177ca577eff988afa
Sara Silva <1140957@isep.ipp.pt>
2016-06-05 15:49:48 +0100

    Modify class Main [Implementation]

--- src/main/java/lapr/project/ui/Main.java
+++ src/main/java/lapr/project/ui/Main.java
@@ -2,12 +2,15 @@ package lapr.project.ui;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import lapr.project.model.exhibitions.CentroExposicoes;
 import lapr.project.model.exhibitions.Exposicao;
 import lapr.project.model.lists.ListaExposicoes;
 import lapr.project.model.lists.ListaFAE;
+import lapr.project.model.lists.ListaOrganizadores;
 import lapr.project.model.lists.RegistoUtilizadores;
 import lapr.project.model.users.Utilizador;
 import lapr.project.utils.Data;
@@ -31,9 +34,9 @@ class Main {
     public static void main(String[] args) {
 
         final CentroExposicoes centroExposicoes = new Main().bootStrap();
-        
+
         new LoginUI(centroExposicoes);
-        
+
         System.out.println(centroExposicoes);
     }
 
@@ -46,7 +49,7 @@ class Main {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("File userlist not found");
         }
-        
+
         Utilizador utilizador1 = new Utilizador("Dummy2", "representante", "representante", "representante@domain.pt", "REPRESENTANTE");
         Utilizador utilizador2 = new Utilizador("Dummy3", "FAE", "FAE", "fae@domain.pt", "FAE");
         Utilizador utilizador3 = new Utilizador("Dummy4", "gestor", "gestor", "gestor@domain.pt", "GESTOR");
@@ -64,15 +67,15 @@ class Main {
 
         final ListaExposicoes listaExposicoes = new ListaExposicoes();
 
-        Exposicao exposicao1 = new Exposicao("exposicao1", "descricao1", new Data(2016, 5, 31), new Data(2016, 7, 29), new Data(2016, 6, 31), new Data(2016, 5, 31), "My House", new ListaFAE());
-        Exposicao exposicao2 = new Exposicao("exposicao2", "descricao2", new Data(2016, 5, 31), new Data(2016, 7, 29), new Data(2016, 6, 31), new Data(2016, 5, 31), "My House", new ListaFAE());
-        Exposicao exposicao3 = new Exposicao("exposicao3", "descricao3", new Data(2016, 5, 31), new Data(2016, 7, 29), new Data(2016, 6, 31), new Data(2016, 5, 31), "My House", new ListaFAE());
+        Exposicao exposicao1 = new Exposicao("exposicao1", "descricao1", new Date(2016, 5, 31), new Date(2016, 7, 29), new Data(2016, 6, 31), new Data(2016, 5, 31), "My House", new ListaOrganizadores(), new ListaFAE(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        Exposicao exposicao2 = new Exposicao("exposicao1", "descricao1", new Date(2016, 5, 31), new Date(2016, 7, 29), new Data(2016, 6, 31), new Data(2016, 5, 31), "My House", new ListaOrganizadores(), new ListaFAE(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        Exposicao exposicao3 = new Exposicao("exposicao1", "descricao1", new Date(2016, 5, 31), new Date(2016, 7, 29), new Data(2016, 6, 31), new Data(2016, 5, 31), "My House", new ListaOrganizadores(), new ListaFAE(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
 
         listaExposicoes.addExposicao(exposicao1);
         listaExposicoes.addExposicao(exposicao2);
         listaExposicoes.addExposicao(exposicao3);
 
-        CentroExposicoes centroExposicoes = new CentroExposicoes(registoUtilizadores, listaExposicoes);
+        CentroExposicoes centroExposicoes = new CentroExposicoes();
 
         return centroExposicoes;
     }
