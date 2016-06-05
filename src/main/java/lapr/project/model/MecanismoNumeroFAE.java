c5ff1507874625b05f05bade784cba0be12baf2b
Sara Silva <1140957@isep.ipp.pt>
2016-06-05 15:46:34 +0100

    Add class MecanismoNumeroFAE [Implementation] [UC-03]

---
+++ src/main/java/lapr/project/model/MecanismoNumeroFAE.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model;
+
+import java.util.List;
+import lapr.project.model.exhibitions.Exposicao;
+
+/**
+ *
+ * @author Sara Silva
+ */
+public class MecanismoNumeroFAE implements MecanismoAtribuicao {
+
+    private String descricao;
+
+    public MecanismoNumeroFAE() {
+        descricao = "Numero FAE";
+    }
+
+    @Override
+    public List<Atribuicao> gerarAtribuicoes(Exposicao exposicao) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
