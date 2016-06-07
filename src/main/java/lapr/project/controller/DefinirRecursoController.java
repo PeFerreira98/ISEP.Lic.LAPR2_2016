a40803653a51b21e638bd39277faf3cbf2dde67c
Sara Silva <1140957@isep.ipp.pt>
2016-06-07 15:38:56 +0100

    Criação da classe DefinirRecursoController [UC-09][Implementation]

---
+++ src/main/java/lapr/project/controller/DefinirRecursoController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.controller;
+
+import java.util.List;
+import lapr.project.model.Recurso;
+import lapr.project.model.exhibitions.CentroExposicoes;
+import lapr.project.model.lists.ListaRecursos;
+
+/**
+ *
+ * @author Sara Silva
+ */
+public class DefinirRecursoController {
+
+    private ListaRecursos listaRecursos;
+
+    public DefinirRecursoController(CentroExposicoes centroExp) {
+        listaRecursos = centroExp.getListaRecursos();
+    }
+
+    public List<Recurso> getListaRecursos() {
+        return listaRecursos.getListaRecursos();
+    }
+
+    public void addRecurso(Recurso rec) {
+        listaRecursos.addRecurso(rec);
+    }
+
+}
