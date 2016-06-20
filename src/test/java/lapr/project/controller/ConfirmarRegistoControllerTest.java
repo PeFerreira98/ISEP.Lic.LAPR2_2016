fcb2fe62fee63af9b93d795ad67db306ef391bd2
TiagoSilvestre <1140948@isep.ipp.pt>
2016-06-20 10:48:23 +0100

    Updated Unit Tests ConfirmarRegistoController

--- src/test/java/lapr/project/controller/ConfirmarRegistoControllerTest.java
+++ src/test/java/lapr/project/controller/ConfirmarRegistoControllerTest.java
@@ -9,6 +9,8 @@ import lapr.project.controller.gestor.ConfirmarRegistoController;
 import java.util.ArrayList;
 import java.util.List;
 import lapr.project.model.exhibitions.CentroExposicoes;
+import lapr.project.model.lists.ListaExposicoes;
+import lapr.project.model.lists.RegistoUtilizadores;
 import lapr.project.model.users.Utilizador;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -93,6 +95,37 @@ public class ConfirmarRegistoControllerTest {
         List<Utilizador> result = ce.getRegistoUtilizadores().getListaUtilizadoresRegistados();
         assertEquals(expResult, result);
 
+    }
+
+    /**
+     * Test of setUtilizador method, of class ConfirmarRegistoController.
+     */
+    @Test
+    public void testSetUtilizador() {
+        System.out.println("setUtilizador");
+        Utilizador utilizador = new Utilizador("Exp", "username", "password", "email");
+        RegistoUtilizadores rUtil = new RegistoUtilizadores();
+        ListaExposicoes lstExpo = new ListaExposicoes();
+        CentroExposicoes centro = new CentroExposicoes(rUtil, lstExpo);
+        ConfirmarRegistoController instance = new ConfirmarRegistoController(centro);
+        instance.setUtilizador(utilizador);
+    }
+
+    /**
+     * Test of utilizadorToString method, of class ConfirmarRegistoController.
+     */
+    @Test
+    public void testUtilizadorToString() {
+        System.out.println("utilizadorToString");
+        Utilizador utilizador = new Utilizador("Exp", "username", "password", "email");
+        RegistoUtilizadores rUtil = new RegistoUtilizadores();
+        ListaExposicoes lstExpo = new ListaExposicoes();
+        CentroExposicoes centro = new CentroExposicoes(rUtil, lstExpo);
+        ConfirmarRegistoController instance = new ConfirmarRegistoController(centro);
+        instance.setUtilizador(utilizador);
+        String expResult = "\nUtilizador{nome=Exp, username=username, password=password, email=email, estado=PENDING}";
+        String result = instance.utilizadorToString();
+        assertEquals(expResult, result);
     }
 
 }
